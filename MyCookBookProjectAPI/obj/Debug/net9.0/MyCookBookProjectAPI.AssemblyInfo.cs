40ce881002d223eea875830ce6b8025b926c4767
Austin Hoang <neo322442@gmail.com>
2025-01-29 11:47:14 -0500

    Finished About Me Page
    Added Image, wrote a summary and links to LinkedIn and GitHub profiles

--- MyCookBookProjectAPI/obj/Debug/net9.0/MyCookBookProjectAPI.AssemblyInfo.cs
+++ MyCookBookProjectAPI/obj/Debug/net9.0/MyCookBookProjectAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCookBookProjectAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d7d323aca18da2c550fe65488ee14808f82f8f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d446dad03195773b169376b9eb2f94dc69bab9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCookBookProjectAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCookBookProjectAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
